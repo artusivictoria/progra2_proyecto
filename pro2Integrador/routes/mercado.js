8619868d192a2fe78a3a72d38707842a9c86e695
sophiadeleone <sdeleone@udesa.edu.ar>
2024-11-11 10:21:06 -0930

    rutas y configuracion app.js

--- pro2Integrador/routes/mercado.js
+++ pro2Integrador/routes/mercado.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', );
+
+router.get('/register', );
+
+router.get('/login', );
+
+router.get('/product', );
 
 module.exports = router;
