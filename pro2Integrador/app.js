b667ddd50389c1865f6dbfaa4bf7d8d15c00e462
sophiadeleone <sdeleone@udesa.edu.ar>
2024-11-11 18:25:07 -0930

    app.js session

--- pro2Integrador/app.js
+++ pro2Integrador/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-//definimos session
+//definimos session aka Requerir express-session en el entry point de la aplicación (app.js)
 const session = require('express-session');
 
 var indexRouter = require('./routes/index');
@@ -22,7 +22,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//justo antes de las rutas -- session
+//Ejecutamos session() pasándole como argumento un objeto literal con la propiedad  secret  con un texto único aleatorio, que servirá para identificar nuestro sitio web.
+app.use(session({
+  secret: "mercadoProyecto", // hash para un usuario  
+  resave: false,
+  saveUninitialized: true
+}));
+      //obs. Implementar Session también deja disponible req.session aka una nueva propiedad dentro del objeto request que está accesible en todo el sitio.
 
+//La asignación de datos a res.locals debe estar dentro del archivo app.js antes de la declaración de las rutas. 
+/*hacmeos un pasamano de la info de session se la enviaos a locals */
+app.use(function(req, res, next){
+  if (req.session.user !=undefined) {   //req.session.user !=undefined es true quiere decir que hay algo guardado
+    res.locals.user= req.session.user  //res.locals es un objeto literal donde puedo almacenar todos los valores que quieran compartir con todas las vistas
+  }
+  return next();
+})
+//obs. con todo esto de sesion luego podes hacer lo de partials de si esta logeado o no mostrar un difernte header ocmo hice en login
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
