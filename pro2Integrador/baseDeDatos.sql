0722d38e26a17020eab144034bd08b2745e47eb6
sophiadeleone <sdeleone@udesa.edu.ar>
2024-11-11 15:25:25 -0930

    inserts productos tabla

--- pro2Integrador/baseDeDatos.sql
+++ pro2Integrador/baseDeDatos.sql
@@ -32,3 +32,10 @@ INSERT INTO users VALUES(DEFAULT, 'victoria@gmail.com', 'victoria' , 'victoriaCo
 INSERT INTO users VALUES(DEFAULT, 'ana@gmail.com', 'ana' , 'anaContrasenia', DEFAULT, DEFAULT, DEFAULT);
 INSERT INTO users VALUES(DEFAULT, 'belen@gmail.com', 'belen' , 'belenContrasenia', DEFAULT, DEFAULT, DEFAULT);
 INSERT INTO users VALUES(DEFAULT, 'catalina@gmail.com', 'catalina' , 'catalinaContrasenia', DEFAULT, DEFAULT, DEFAULT);
+
+INSERT INTO products VALUES(DEFAULT, 'https://mmgrim2.azureedge.net/MediaFiles/Grimoldi/2024/10/2/10042902_800.jpg', 'cartera negra' , 'Cartera grimoldi cuero Mercurio', DEFAULT, DEFAULT, DEFAULT, 1)
+INSERT INTO products VALUES(DEFAULT, 'https://mmgrim2.azureedge.net/MediaFiles/Grimoldi/2024/8/22/9926336_800.jpg', 'mochila negra' , 'Mochila grimoldi Concept X ', DEFAULT, DEFAULT, DEFAULT, 1)
+INSERT INTO products VALUES(DEFAULT, 'https://newswarovskiargentina.vteximg.com.br/arquivos/ids/641748-1000-1000/5662179_ms1.jpg', 'collar Lilia' , 'Collar swarovski en Y', DEFAULT, DEFAULT, DEFAULT, 2)
+INSERT INTO products VALUES(DEFAULT, 'https://ar.isadoraonline.com/media/catalog/product/4/8/48058602_0_1_20220905180506.jpg', 'aros argollas', 'Aros de plata', DEFAULT, DEFAULT, DEFAULT, 3)
+INSERT INTO products VALUES(DEFAULT, 'https://ar.isadoraonline.com/media/catalog/product/4/8/48760701_0_1_20240903150812.jpg', 'pañuelo flores' , 'pañuelo isadora de verano', DEFAULT, DEFAULT, DEFAULT, 4)
+
