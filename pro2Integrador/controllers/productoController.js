a36406eaef1ce2b652759bef13c8ef20026d6c73
artusivictoria <vartusi@udesa.edu.ar>
2024-11-14 10:35:04 -0300

    agregar producto, productoController y ejs formulario

--- pro2Integrador/controllers/productoController.js
+++ pro2Integrador/controllers/productoController.js
@@ -7,7 +7,32 @@ const productoController = {
 
     product: (function(req, res){
         res.render('product')
-    })
+    }),
+
+    showFormCreate: function (req, res) {
+        return res.render("product-add");
+      },
+
+    store: function (req, res) {
+        //proceso los datos que vienen del formulario
+    
+        let producto = req.body;
+        //return res.send(producto)
+    
+        db.Product.create(producto)
+    
+        .then(function(results){
+          return res.redirect('/mercado');
+        })
+        .catch(function (err) {
+          console.log(err)
+        })
+    
+    
+        //primer paso siempre veo los datos que vienen del formulario crear
+        //return res.send(req.body) 
+      }
+
 
 };
 
--- pro2Integrador/routes/mercado.js
+++ pro2Integrador/routes/mercado.js
@@ -7,4 +7,10 @@ router.get('/', mercadoController.index);
 
 router.get('/product', mercadoController.product);
 
+/* GET crear producto */
+router.get('/register', productoController.showFormCreate);
+//ruta de post producto
+router.post('/register' , prroductoController.store)
+
+
 module.exports = router;
